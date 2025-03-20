f107970aaba57eeed941f1e615a31415949d63b9
Abhishek Upadhya <a.upadhya05@gmail.com>
2025-03-20 15:34:44 +0530

    Added shadow_v2.py and changed my_algo.py to shadow_v1.py

--- Abhishek/config.py
+++ Abhishek/config.py
@@ -1,5 +1,5 @@
-from teams import my_algo, default_algo1
+from teams import shadow_v1, default_algo1, default_algo2, shadow_v2
 
-TEAM1 = my_algo
+TEAM1 = shadow_v2
 TEAM2 = default_algo1
 VALUE_ERROR = False
--- Abhishek/teams/shadow_v1.py
+++ Abhishek/teams/shadow_v1.py
@@ -1,9 +1,9 @@
 import random
 from teams.helper_function import Troops, Utils
 
-team_name = "Abhishek"
+team_name = "Shadow_V1"
 troops = [
-    Troops.skeleton, Troops.dragon, Troops.valkyrie, Troops.archer,
+    Troops.minion, Troops.dragon, Troops.valkyrie, Troops.archer,
     Troops.prince, Troops.wizard, Troops.giant, Troops.knight
 ]
 # Total Elixr cost = 3 + 4 + 4 + 3 + 5 + 5 + 5 + 3 = 32
@@ -26,7 +26,12 @@ def deploy(arena_data: dict):
 def logic(arena_data: dict):
     global team_signal
     my_tower = arena_data["MyTower"]
+    my_troops = arena_data["MyTroops"]
     opp_troops = arena_data["OppTroops"]
+    
+    # If game just started, wait for sometime
+    if my_tower.game_timer < 1:
+        return
     
     elixr = my_tower.total_elixir # Current elixr
     
@@ -42,26 +47,38 @@ def logic(arena_data: dict):
     "Valkyrie": ["Wizard", "Dragon", "Minion", "Prince"],
     "Musketeer": ["everything"],
     "Giant": ["Wizard", "Skeleton", "Barbarian", "Minion", "Prince"],
-    "Prince": ["Dragon", "Skeleton", "Minion"],
-    "Barbarian": ["Wizard", "Dragon", "Valkyrie", "Minion"],
+    "Prince": ["Dragon", "Wizard", "Skeleton", "Minion"],
+    "Barbarian": ["Wizard", "Dragon", "Minion", "Prince"],
     "Balloon": ["Archer", "Wizard", "Dragon", "Musketeer", "Minion"],
-    "Wizard": ["Wizard", "Prince", "Dragon", "Knight"]
+    "Wizard": ["Giant", "Wizard", "Prince", "Dragon"]
     }
     
     # Getting opponent troop data
     opp_troop_data = {}
     for troop in opp_troops:
         opp_troop_data[troop.name] = troop.position
+        
+    # Getting my troop data
+    my_troop_data = {}
+    for troop in my_troops:
+        my_troop_data[troop.name] = troop.position
     
     # Checking weakness and deploying
-    flag = True
+    flag = True # Is true if we have already reacted to opponent's weakness or ignored it
     for troop_name in opp_troop_data.keys():
         if weakness[troop_name][0] == "everything":
-            flag = True
             continue
+            
+        # Not react if we have already dealt with weakness and weakness not wizard!
+        if set(my_troop_data.keys()) & set(weakness[troop_name]) and elixr < 4:
+            if "Wizard" != troop_name:
+                # print(troop_name, "weakness dealt with", set(my_troop_data.keys()) & set(weakness[troop_name]))
+                continue
+            
+        # Check if we can deal with weakness
         for mirror in weakness[troop_name]:
-            if mirror in deployable and opp_troop_data[troop_name][1] <= 35:
-                deploy_list.list_.append((mirror, (opp_troop_data[troop_name][0], 0)))
+            if mirror in deployable and opp_troop_data[troop_name][1] <= 40:
+                deploy_list.list_.append((mirror, (opp_troop_data[troop_name][0], max(opp_troop_data[troop_name][1]-10, 0))))
                 flag = False
                 break
             if mirror in deployable and elixr >= 9.5:
@@ -70,22 +87,18 @@ def logic(arena_data: dict):
                 break
         if not flag:
             break
-    
-    if elixr >= 9.5 and flag:
-        while elixr >= 4:
-            if "Wizard" in deployable:
-                deploy_list.list_.append(("Wizard", (0, 0)))
-                elixr -= 5
-            elif "Giant" in deployable:
-                deploy_list.list_.append(("Giant", (0, 0)))
-                elixr -= 5
-            else:
-                deploy_list.list_.append((deployable[0], (0, 0)))
+
+    # If we have dealt with all weaknesses, we shall go on the offensive
+    if flag:
+        if "Giant" in my_troop_data.keys():
+            if my_troop_data["Giant"][1] > 50:
+                deploy_list.list_.append((deployable[0], (my_troop_data["Giant"][0], 49)))
                 elixr -= 4
-        
-        
-        
-        
-        
-        
-        
+        if elixr >= 9.7 and "Giant" in deployable:
+            deploy_list.list_.append(("Giant", (0, 25)))
+            elixr -= 5
+        if elixr >= 5:
+            for deploy in deployable:
+                if deploy != "Giant":
+                    deploy_list.list_.append((deploy, (0 ,0)))
+
