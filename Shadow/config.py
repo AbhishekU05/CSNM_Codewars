f467aec98bab39d72f1f21db046b219ce206a359
Abhishek Upadhya <a.upadhya05@gmail.com>
2025-03-22 12:30:39 +0530

    Added shadow-v4

--- Shadow/config.py
+++ Shadow/config.py
@@ -1,5 +1,5 @@
-from teams import shadow_v1, default_algo1, default_algo2, shadow_v2, shadow_v3, tester
+from teams import shadow_v1, default_algo1, default_algo2, shadow_v2, shadow_v3, tester, shadow_v4
 
-TEAM1 = shadow_v3
+TEAM1 = shadow_v4
 TEAM2 = default_algo1
 VALUE_ERROR = False
