c20ad1940f35b9629090a6a23e6742aa1fc83b4f
Abhishek Upadhya <a.upadhya05@gmail.com>
2025-03-24 17:22:05 +0530

    Added shadow_v5

--- Shadow/config.py
+++ Shadow/config.py
@@ -1,5 +1,5 @@
-from teams import shadow_v1, default_algo1, default_algo2, shadow_v2, shadow_v3, tester, shadow_v4, test_wit_abhisheikh
+from teams import shadow_v1, default_algo1, default_algo2, shadow_v2, shadow_v3, tester, shadow_v4, shadow_v5 
 
-TEAM1 = shadow_v4
-TEAM2 = test_wit_abhisheikh
+TEAM1 = shadow_v5
+TEAM2 = shadow_v4
 VALUE_ERROR = False
